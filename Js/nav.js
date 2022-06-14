565f15890d6d42bc2d06a268a4659193d244d0fa
manojpangle05 <manojpangle05@gmail.com>
2022-06-14 14:27:45 +0530

    files upload

---
+++ Js/nav.js
@@ -0,0 +1,24 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".marg");
+
+const sectionOneOptions = {
+  rootMargin: "-290px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+
